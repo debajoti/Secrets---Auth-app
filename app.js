d521aad84e95e1a1d27dce40aae2a92e73a0ef55
Debajoti Chakraborty <chakrabortydebajoti@gmail.com>
2023-09-02 23:15:41 +0530

    bycrypt

---
+++ app.js
@@ -0,0 +1,74 @@
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+    email : String,
+    password : String
+})
+// console.log(process.env.SECRET);
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/",function(req,res){
+    res.render("home");
+}) 
+app.get("/login",function(req,res){
+    res.render("login");
+}) 
+app.get("/register",function(req,res){
+    res.render("register");
+}) 
+
+app.post("/register", function(req,res){
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password : hash
+        });
+    
+        try {
+            newUser.save();
+            res.render("secrets");
+        } catch (err) {
+            console.log(err);
+        }
+    });
+    
+});
+
+app.post("/login", async function(req,res){
+    const username = await req.body.username;
+    const password = await req.body.password;
+
+    let foundUser = await User.findOne({email: username})
+    try{
+            if(foundUser){
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result === true){
+                        res.render("secrets");
+                    }
+                });
+            }
+    } catch{
+        console.log(err);
+    }
+});
+app.get("/logout",function(req,res){
+    res.render("home");
+})
+app.listen(3000, function(){
+    console.log("Server started on 3000");
+})
